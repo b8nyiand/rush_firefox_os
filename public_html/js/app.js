1da48498e34030c9ab50e2b5d12ef208ac52ab7e
András <Andrs@Bandi_TOSHIBA>
2015-11-04 17:53:56 +0100

    added new views

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,27 +1,75 @@
-/*function clickButton(e){
-    e.target.innerHTML = "kattintott";
-}
-document.addEventListener("DOMContentLoaded", function (event) {
-   console.log("DOM loaded and parsed"); 
-   document.querySelector("button").addEventListener("click", clickButton);
-});
-*/
+//navigating from dashboard to the list of upcoming tasks, and back
 
 document.querySelector('#btn-show-tasks').addEventListener('click', function () {
     document.querySelector('#show-tasks').className = 'current';
     document.querySelector('[data-position="current"]').className = 'left';
-     /*var battery = navigator.battery; 
-    if (battery) {
-        var batteryLevel = Math.round(battery.level * 100) + "%";
-        var charging = (battery.charging) ? "Yes" : "No";         var chargingTime = parseInt(battery.chargingTime / 60, 10);         var dischargingTime = parseInt(battery.dischargingTime / 60, 10);
-
-        document.querySelector('#battery-level').textContent = batteryLevel;
-        document.querySelector('#battery-charging').textContent = charging;
-        document.querySelector('#battery-time-charge').textContent = chargingTime;
-        document.querySelector('#battery-time-discharge').textContent = dischargingTime;     }*/
 });
 
 document.querySelector('#btn-back').addEventListener('click', function () {
     document.querySelector('#show-tasks').className = 'right';
     document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//navigating from dashboard to the add task view, and back
+
+document.querySelector('#btn-add-task').addEventListener('click', function () {
+    document.querySelector('#add-task').className = 'current';
+    document.querySelector('[data-position="current"]').className = 'left';
+});
+
+document.querySelector('#btn-back1').addEventListener('click', function () {
+    document.querySelector('#add-task').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//navigating from dashboard to the achievements view, and back
+
+document.querySelector('#btn-show-achievements').addEventListener('click', function () {
+    document.querySelector('#show-achievements').className = 'current';
+    document.querySelector('[data-position="current"]').className = 'left';
+});
+
+document.querySelector('#btn-back2').addEventListener('click', function () {
+    document.querySelector('#show-achievements').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//navigating from dashboard to the profile and statistics view, and back
+
+document.querySelector('#btn-show-profile-stats').addEventListener('click', function () {
+    document.querySelector('#show-profile-stats').className = 'current';
+    document.querySelector('[data-position="current"]').className = 'left';
+});
+
+document.querySelector('#btn-back3').addEventListener('click', function () {
+    document.querySelector('#show-profile-stats').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//navigating from dashboard to the settings view, and back
+
+document.querySelector('#btn-settings').addEventListener('click', function () {
+    document.querySelector('#settings').className = 'current';
+    document.querySelector('[data-position="current"]').className = 'left';
+});
+
+document.querySelector('#btn-back4').addEventListener('click', function () {
+    document.querySelector('#settings').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+});
+
+//navigating from dashboard to the exit app view
+
+document.querySelector('#btn-exit-app').addEventListener('click', function () {
+    document.querySelector('#exit-app').className = 'current';
+    document.querySelector('[data-position="current"]').className = 'left';
+});
+
+document.querySelector('#btn-exit-no').addEventListener('click', function () {
+    document.querySelector('#exit-app').className = 'right';
+    document.querySelector('[data-position="current"]').className = 'current';
+});
+
+document.querySelector('#btn-exit-yes').addEventListener('click', function () {
+    window.close();
 });
